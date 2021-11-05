9715d980ed11beb9ccb5eec5bd17799aaf47b694
Niharika <niharikakrishnan@gmail.com>
2021-11-04 22:30:41 -0400

    Normalised and tested with images

--- canny.py
+++ canny.py
@@ -6,85 +6,88 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 
-def convolution(image, kernel, normalisation):
-	image_row, image_col = image.shape
-	kernel_row, kernel_col = kernel.shape
-	output = np.zeros(image.shape)
-
-	pad_height = int((kernel_row - 1) / 2)
-	pad_width = int((kernel_col - 1) / 2)
-	print("pad_height; ", pad_height, "pad_width", pad_width)
-
-	padded_image = np.zeros((image_row + (2 * pad_height), image_col + (2 * pad_width)))
-	padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
-
-	print("padded_image", padded_image)
-	for row in range(1,image_row-1):
-		for col in range(1,image_col-1):
-			output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
-			if normalisation:
-				output[row, col] /= 140
-	print(output)
-	return output
-
-def gaussian_smoothing(image, kernel, display):
+def convolution(image, mask):
 	
-	#Checking if image is color or grayscale format and converting to grayscale format
-	print(image.shape)
-	if len(image.shape) == 3:
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#Getting the number of rows and columns of the image and the mask
+	image_row, image_col = image.shape
+	mask_row, mask_col = mask.shape
+	print("Convoluting image of shape {} with kernel size of {}".format(image.shape, mask.shape))
+	
+	#Initialising the convoluted 2D array with zeroes
+	convoluted_image = np.zeros(image.shape)
 
-		print("Converted to Gray Channel. Size : {}".format(image.shape))
+	padding_height = int((mask_row - 1) / 2)
+	padding_width = int((mask_col - 1) / 2)
+
+	padded_image = np.zeros((image_row + (2 * padding_height), image_col + (2 * padding_width)))
+	padded_image[padding_height:padded_image.shape[0] - padding_height, padding_width:padded_image.shape[1] - padding_width] = image
+
+	#Matrix multiplication - Convoluting image with kernel
+	for row in range(1, image_row-1):
+		for col in range(1, image_col-1):
+			convoluted_image[row, col] = np.sum(mask * padded_image[row:row + mask_row, col:col + mask_col])
+	
+	return convoluted_image
+
+def gaussianSmoothing(image, mask):
+	print("Applying Gaussian Smoothing to input image")
+	
+	image_row, image_col = image.shape
 
 	#Applying Gaussian Smoothing to the input image
-	gaussian_image = convolution(image, kernel, normalisation=True)
-	
-	if display:
-		plt.imshow(gaussian_image)
-		plt.title("Output Image using {}X{} Kernel".format(kernel.shape[0], kernel.shape[1]))
-		plt.show()
+	gaussian_image = convolution(image, mask)
+
+	#Normalising the gaussian smoothened image by using the sum of total pixels
+	for row in range(1, image_row-1):
+		for col in range(1, image_col-1):
+			if gaussian_image[row, col] > 255:
+				gaussian_image[row, col] /= 140
 	return gaussian_image
 
-def gradient_operation(image, edge_filter, convert_to_degree, display):
+def gradientOperation(image, edge_filter):
+	print("Computing Horizontal Gradient of the Smoothened Image.")
+	image_row, image_col = image.shape
 
-	horizontal_gradient = convolution(image, edge_filter, normalisation=False)
-	
-	if display:
-		plt.imshow(horizontal_gradient)
-		plt.title("Normalised horizontal gradient using Prewitt Operator")
-		plt.show()
+	#Computing the horizontal gradient
+	horizontal_gradient = convolution(image, edge_filter)
 
-	vertical_gradient = convolution(image, np.flip(edge_filter.T, axis=0), normalisation=False)
-	
-	if display:
-		plt.imshow(vertical_gradient)
-		plt.title("Normalised vertical gradient using Prewitt Operator")
-		plt.show()
+	print("Computing Vertical Gradient.")
+	#Transforming and flipping the horizontal gradient edge_filter to calculate vertical gradient
+	vertical_edge_filter = np.flip(edge_filter.T, axis=0)
+	vertical_gradient = convolution(image, vertical_edge_filter)
 
-	#gradient_magnitude = abs(horizontal_gradient) + abs(vertical_gradient)
-
+	print("Computing Gradient Magnitude.")
 	#Using the formula, gradient magnitude = Square Root of Squares of Horizontal and Vertical Gradient
 	gradient_magnitude = np.sqrt(np.square(horizontal_gradient) + np.square(vertical_gradient))
-	
+
+	#Normalising the Gradient Magnitude by 1/3 of the original value
+	for row in range(1, image_row-1):
+		for col in range(1, image_col-1):
+			if gradient_magnitude[row, col] > 255:
+				gradient_magnitude[row, col] /= 3
+
+	print("Computing Gradient Angle.")
 	#Calculating gradient angle -> tan inverse (vertical gradient/horizontal gradient) in radians
-	gradient_direction = np.arctan2(vertical_gradient, horizontal_gradient)
+	gradient_angle = np.arctan2(vertical_gradient, horizontal_gradient)
 	
+	print("Computing Gradient Direction.")
 	#Converting gradient angle from radians to degree. Required to perform non-maxima suppression
-	gradient_direction = np.rad2deg(gradient_direction)
-	gradient_direction += 180
+	gradient_direction = np.rad2deg(gradient_angle)
 
-	print(gradient_direction)
-	return gradient_magnitude, gradient_direction, vertical_gradient, horizontal_gradient
+	return horizontal_gradient, vertical_gradient, gradient_magnitude, gradient_direction 
 
-def non_maxima_suppression(gradient_magnitude, gradient_direction, display):
-	image_row, image_col = gradient_magnitude.shape
-	output = np.zeros(gradient_magnitude.shape)
-	sector = np.zeros(gradient_magnitude.shape)
+def nonMaximaSuppression(gradient_magnitude, gradient_direction):
+	print("Applying Non Maxima Suppression")
+	gradient_row, gradient_col = gradient_magnitude.shape
 	
-	#Applying non maima suppression to all pixels other than the border
-	for row in range(1, image_row - 1):
-		for col in range(1, image_col - 1):
+	nms_output = np.zeros(gradient_magnitude.shape)
+	
+	#Applying non maxima suppression to all pixels other than the border
+	for row in range(1, gradient_row-1):
+		for col in range(1, gradient_col-1):
 			angle = gradient_direction[row, col]
+			
+			angle += 180
 
 			#Mapping to Sector 0
 			if (0 <= angle < 22.5) or (337.5 <= angle <= 360):
@@ -107,101 +110,91 @@ def non_maxima_suppression(gradient_magnitude, gradient_direction, display):
 				after_pixel = gradient_magnitude[row + 1, col + 1]
 
 			if ((gradient_magnitude[row, col] > before_pixel) and (gradient_magnitude[row, col] > after_pixel)):
-				output[row, col] = gradient_magnitude[row, col]
+				nms_output[row, col] = gradient_magnitude[row, col]
+	return nms_output
 
-		#print(sector[row])
-	if display:
-		plt.imshow(output)
-		plt.title("Output Image with Non Maxima Suppression")
-		plt.show()
-
-	print(output)
-	return output
-
-def threshold(image, display):
-	image_row, image_col = image.shape
-	output = np.zeros(image.shape)
+def simpleThresholding(image):
+	print("Applying Simple Thresholding")
 	
-	threshold_25 = np.quantile(image.flatten(), 0.25)
-	threshold_50 = np.quantile(image.flatten(), 0.50)
-	threshold_75 = np.quantile(image.flatten(), 0.75)
-	print(threshold_25)
-	print(threshold_50)
-	print(threshold_75)
+	image_row, image_col = image.shape
+	thresholding = {}
 
-	threshold = [threshold_25, threshold_50, threshold_75]
+	threshold_25 = np.percentile(list(set(image.flatten())), 25)
+	threshold_50 = np.percentile(list(set(image.flatten())), 50)
+	threshold_75 = np.percentile(list(set(image.flatten())), 75)
+	threshold = {"threshold_25": threshold_25, "threshold_50": threshold_50, "threshold_75": threshold_75}
 
-	for threshold_value in threshold:
+	thresholding_output = {}
+
+	#Applying simple thresholding with threshold values at 25, 50 and 75th percentile
+	for threshold_key, threshold_value in threshold.items():
+		output = np.zeros(image.shape)
+
 		for row in range(image_row):
 			for col in range(image_col):
-				if image[row, col] < threshold_value:
-					output[row, col] = 0
-				else:
+				if image[row, col] > threshold_value:
 					output[row, col] = 255
-		if display:
-			plt.imshow(output)
-			plt.title("Output Image with threshold")
-			plt.show()
-
-	print(output)
-	return output
-
+		thresholding_output[threshold_key] = output
+	return thresholding_output
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True, help="Path to the image")
-    args = vars(ap.parse_args())
-    print(args)
+	print("Running Canny Edge Detector!")
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True, help="Path to the image")
+	args = vars(ap.parse_args())
 
-    #frame = cv2.imread('images/house.bmp')
-    #image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#Reading and opening input image and coverting to gray scale
+	frame = cv2.imread(args['image'])
+	image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    frame = cv2.imread(args['image'])
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#Creating path to write output images of Canny Edge Detector
+	folder, fname_with_extension = os.path.split(args['image'])
+	fname , extension = os.path.splitext(fname_with_extension)
+	path = str(fname) + "_output"
+	access = 0o755
 
-    folder, fname_with_extension = os.path.split(args['image'])
-    print(folder)
-    print(fname_with_extension)
+	#Checking whether the specified output path exists or not
+	isExist = os.path.exists(path)
 
-    fname , extension = os.path.splitext(fname_with_extension)
-    print(fname)
+	#Creating a new directory if it does not already exist
+	if not isExist:
+		os.makedirs(path,access)
+		print("Output directory created.")
+
+	#Defining 7 x 7 Gaussian mask as mentioned in the Project requirement
+	mask = np.array(
+        [[1, 1, 2, 2, 2, 1, 1], 
+         [1, 2, 2, 4, 2, 2, 1], 
+         [2, 2, 4, 8, 4, 2, 2], 
+         [2, 4, 8, 16, 8, 4, 2], 
+         [2, 2, 4, 8, 4, 2, 2],
+         [1, 2, 2, 4, 2, 2, 1], 
+         [1, 1, 2, 2, 2, 1, 1]], dtype='int')
+	
+	#Applying Gaussian smoothing to input image with given mask
+	gaussian_image = gaussianSmoothing(image, mask)
+	cv2.imwrite(path + "/"+str(fname)+"_GaussianSmoothing.bmp", gaussian_image)
+
+	#Defining Prewitt's Edge Operator
+	edge_filter = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], dtype='int')
+
+	#Applying gradient operation to compute horizontal gradient, vertical gradient, gradient magnitude and gradient angle
+	horizontal_gradient, vertical_gradient, gradient_magnitude, gradient_direction = gradientOperation(image, edge_filter)
+	cv2.imwrite(path + "/"+str(fname)+"_HorizontalGradient.bmp", horizontal_gradient)
+	cv2.imwrite(path + "/"+str(fname)+"_VerticalGradient.bmp", vertical_gradient)
+	cv2.imwrite(path + "/"+str(fname)+"_GradientMagnitude.bmp", gradient_magnitude)
+
+	#Applying Non Maima Suppression to the gradient magnitude
+	nonmaxima_image = nonMaximaSuppression(gradient_magnitude, gradient_direction)
+	cv2.imwrite(path + "/"+str(fname)+"_NonMaximaSuppression.bmp", nonmaxima_image)
+
+	#Applying Simple thresholding with thresholds chosen at 25th, 50th and 75th percentile
+	threshold_image = simpleThresholding(nonmaxima_image)
+	for threshold_name, threshold_value in threshold_image.items():
+		cv2.imwrite(path + "/"+str(fname)+ "_"+threshold_name+".bmp", threshold_value)
+
+	print("Canny Edge Detector Implemented and output images stored in the directory!")
 
 
-    path = str(fname) + "_Output"
-    access = 0o755
-
-    # Check whether the specified path exists or not
-    isExist = os.path.exists(path)
-
-    if not isExist:
-        # Create a new directory because it does not exist
-        os.makedirs(path,access)
-        print("The new directory is created!")
-
-
-    edge_filter = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], dtype='int')
-
-
-
-    kernel = np.array(
-        [[1, 1, 2, 2, 2, 1, 1], [1, 2, 2, 4, 2, 2, 1], [2, 2, 4, 8, 4, 2, 2], [2, 4, 8, 16, 8, 4, 2], [2, 2, 4, 8, 4, 2, 2],
-         [1, 2, 2, 4, 2, 2, 1], [1, 1, 2, 2, 2, 1, 1]], dtype='int')
-
-    gaussian_image = gaussian_smoothing(image, kernel, display=True)
-    save_path = path + "/"+str(fname)+"_GaussianSmoothing.bmp"
-    plt.imsave(save_path, gaussian_image)
-
-
-    gradient_magnitude, gradient_direction,vertical_gradient, horizontal_gradient = gradient_operation(image, edge_filter, convert_to_degree=True, display=True)
-    plt.imsave(path + "/"+str(fname)+"_VerticalGradient.bmp", vertical_gradient)
-    plt.imsave(path + "/"+str(fname)+"_HorizontalGradient.bmp", horizontal_gradient)
-    plt.imsave(path + "/"+str(fname)+"_GradientMagnitude.bmp", gradient_magnitude)
-
-
-
-    nms_image = non_maxima_suppression(gradient_magnitude, gradient_direction, display=True)
-    plt.imsave(path + "/"+str(fname)+"_GradientMagnitude_NMS.bmp", nms_image)
-
-    # threshold_image = threshold(nms_image, display=True)
 
 
