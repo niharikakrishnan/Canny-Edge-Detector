95d44f70f777bdfda5c13ebcc31dfaec993ea0b5
Niharika <niharikakrishnan@gmail.com>
2021-11-05 11:38:54 -0400

    Relevant comments added. Modified normalisation

--- canny.py
+++ canny.py
@@ -1,14 +1,20 @@
-import numpy as np
-import math
 from math import degrees, pi
-import cv2
-import matplotlib.pyplot as plt
+import numpy as np
 import argparse
+import cv2
 import os
 
 def convolution(image, mask):
+	'''
+	Function to perform convolution of an image with a mask using mtri multiplication. In cases where the mask goes 
+	outside of the image border, it is considered as undefined and replaced with zeroes. Region of interest surrounding 
+	every reference pixel is computed and multiplied with the mask
+	:param image:
+	:param mask: 
+	:return convoluted_image: Convoluted Image with shape same as the input image
+	'''
 	
-	#Getting the number of rows and columns of the image and the mask
+	#Getting the number of rows and columns of the image and the mask using .shape method
 	image_row, image_col = image.shape
 	mask_row, mask_col = mask.shape
 	print("Convoluting image of shape {} with kernel size of {}".format(image.shape, mask.shape))
@@ -16,20 +22,33 @@ def convolution(image, mask):
 	#Initialising the convoluted 2D array with zeroes
 	convoluted_image = np.zeros(image.shape)
 
-	padding_height = int((mask_row - 1) / 2)
-	padding_width = int((mask_col - 1) / 2)
+	#Defining the number of rows and columns that will be undefined based on the mask dimensions 
+	height = (mask_row - 1) // 2
+	width = (mask_col - 1) // 2
+	
+	#Initialising a 2D array with zeroes along with extra rows and columns to handle the undefine values
+	modified_image = np.zeros((image_row + (2 * height), image_col + (2 * width)))
 
-	padded_image = np.zeros((image_row + (2 * padding_height), image_col + (2 * padding_width)))
-	padded_image[padding_height:padded_image.shape[0] - padding_height, padding_width:padded_image.shape[1] - padding_width] = image
+	modified_image_height, modified_image_width = modified_image.shape
+	
+	#Defining the region of interest for the input image
+	modified_image[height: modified_image_height  - height, width:modified_image_width - width] = image
 
 	#Matrix multiplication - Convoluting image with kernel
 	for row in range(1, image_row-1):
 		for col in range(1, image_col-1):
-			convoluted_image[row, col] = np.sum(mask * padded_image[row:row + mask_row, col:col + mask_col])
+			#Using sliding window concept for maxtrix multiplication of kernel and region of interest of input image
+			convoluted_image[row, col] = np.sum(mask * modified_image[row : row + mask_row, col : col + mask_col])
 	
 	return convoluted_image
 
 def gaussianSmoothing(image, mask):
+	'''
+	
+	:param image:
+	:param mask: 
+	:return gauusian_image: 
+	'''
 	print("Applying Gaussian Smoothing to input image")
 	
 	image_row, image_col = image.shape
@@ -42,18 +61,29 @@ def gaussianSmoothing(image, mask):
 		for col in range(1, image_col-1):
 			if gaussian_image[row, col] > 255:
 				gaussian_image[row, col] /= 140
+	
 	return gaussian_image
 
 def gradientOperation(image, edge_filter):
+	'''
+	
+	:param image:
+	:param edge_filter: 
+	:return horizontal_gradient:
+	:return vertical_gradient:
+	:return gradient_magnitude:
+	:return gradient_direction: 
+	'''
 	print("Computing Horizontal Gradient of the Smoothened Image.")
 	image_row, image_col = image.shape
 
-	#Computing the horizontal gradient
+	#Computing the horizontal gradient by convoluting the input image with prewitt's horizontal edge filter
 	horizontal_gradient = convolution(image, edge_filter)
 
 	print("Computing Vertical Gradient.")
 	#Transforming and flipping the horizontal gradient edge_filter to calculate vertical gradient
 	vertical_edge_filter = np.flip(edge_filter.T, axis=0)
+	#Computing the vertical gradient by convoluting the input image with prewitt's vertical edge filter
 	vertical_gradient = convolution(image, vertical_edge_filter)
 
 	print("Computing Gradient Magnitude.")
@@ -77,6 +107,12 @@ def gradientOperation(image, edge_filter):
 	return horizontal_gradient, vertical_gradient, gradient_magnitude, gradient_direction 
 
 def nonMaximaSuppression(gradient_magnitude, gradient_direction):
+	'''
+	
+	:param gradient_magnitude:
+	:param gradient_direction:
+	:return nms_output:
+	'''
 	print("Applying Non Maxima Suppression")
 	gradient_row, gradient_col = gradient_magnitude.shape
 	
@@ -87,33 +123,40 @@ def nonMaximaSuppression(gradient_magnitude, gradient_direction):
 		for col in range(1, gradient_col-1):
 			angle = gradient_direction[row, col]
 			
+			#Adding 180degrees to gradient angle for handling negative gradient angles and ease of sector calculation 
 			angle += 180
 
-			#Mapping to Sector 0
+			#Mapping to Sector 0, hence compare with left and right pixels
 			if (0 <= angle < 22.5) or (337.5 <= angle <= 360):
 				before_pixel = gradient_magnitude[row, col - 1]
 				after_pixel = gradient_magnitude[row, col + 1]
 
-			#Mapping to Sector 1
+			#Mapping to Sector 1, hence compare with upper right and lower left pixels
 			elif (22.5 <= angle < 67.5) or (202.5 <= angle < 247.5):
 				before_pixel = gradient_magnitude[row + 1, col - 1]
 				after_pixel = gradient_magnitude[row - 1, col + 1]
 
-			#Mapping to Sector 2
+			#Mapping to Sector 2, hence compare with upper and lower pixels
 			elif (67.5 <= angle < 112.5) or (247.5 <= angle < 292.5):
 				before_pixel = gradient_magnitude[row - 1, col]
 				after_pixel = gradient_magnitude[row + 1, col]
 			
-			#Mapping to Sector 3
+			#Mapping to Sector 3, hence compare with upper left and lower right pixels
 			else:
 				before_pixel = gradient_magnitude[row - 1, col - 1]
 				after_pixel = gradient_magnitude[row + 1, col + 1]
 
+			#If the centre pixel is strictly greater than the neighbouring pixels, we use the gradient magnitude value, else zero
 			if ((gradient_magnitude[row, col] > before_pixel) and (gradient_magnitude[row, col] > after_pixel)):
 				nms_output[row, col] = gradient_magnitude[row, col]
 	return nms_output
 
 def simpleThresholding(image):
+	'''
+
+	:param image:
+	:return tresholding_output: 
+	'''
 	print("Applying Simple Thresholding")
 	
 	image_row, image_col = image.shape
